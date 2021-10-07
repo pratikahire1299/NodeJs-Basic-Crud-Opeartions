26422bd2314c8d1074d1428fd1456386078b9447
pratikahire1299 <73336913+pratikahire1299@users.noreply.github.com>
2021-10-07 16:21:50 +0530

    Add files via upload

---
+++ ecmafile.js
@@ -0,0 +1,6 @@
+//import { simple } from "./mjsfile.mjs"
+//simple()
+
+import  * as temp from "./mjsfile.mjs";
+
+temp.simple2()
---
+++ mjsfile.mjs
@@ -0,0 +1,7 @@
+export function simple(){
+    console.log("Simple Function")
+}
+
+export function simple2(){
+    console.log("Simple two function")
+}
